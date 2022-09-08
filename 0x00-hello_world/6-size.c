31b641fbb972b3734d595fa8ecf8357c31afaf17
Mackay Brian <brianmackay091@gmail.com>
2022-09-08 10:41:36 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,6 @@ int main(void)
 		printf("Size of an int: %ld byte(s)\n", sizeof(i));
 		printf("Size of long int: %ld byte(s)\n", sizeof(li));
 		printf("Size of long long: %ld byte(s)\n", sizeof(ll));
-		printf("Size of long long: %ld byte(s)\n", sizeof(ll));
+		printf("Size of float: %ld byte(s)\n", sizeof(f));
 		return (0);
 	}
