6de8f27746c634117950324b3c234b607dc066cb
Mackay Brian <brianmackay091@gmail.com>
2022-09-08 09:44:01 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints size of data types
+ *
+ * Return : 0
+ */
+int main(void)
+	{
+		char c;
+		int i;
+		long int li;
+		long long ll;
+		float f;
+
+		printf("size of char: %ld byte(s)\n", sizeof(c));
+	        printf("size of int: %ld byte(s)\n", sizeof(i));
+		printf("size of long int: %ld byte(s)\n", sizeof(li));
+ 		printf("size of long long: %ld byte(s)\n", sizeof(ll));
+		printf("size of float: %ld byte(s)\n", sizeof(f));	
+
+		return (0);
+	}
