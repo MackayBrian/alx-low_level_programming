caa4af4f2320c0863be719ce7fe6653abf464c6d
Mackay Brian <brianmackay091@gmail.com>
2022-09-09 05:46:11 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main-print single digit numbers from 0
+ *
+ * Return: always 0
+ */
+int main(void)
+	{
+		int i;
+
+		for (i = 0; i < 10; i++)
+			printf("%i", i);
+			printf("\n");
+		return (0);
+	}
