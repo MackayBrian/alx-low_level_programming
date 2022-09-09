9967f6646aa0caa7b66b2a2f1cff7d47ce57c30e
Mackay Brian <brianmackay091@gmail.com>
2022-09-09 06:18:01 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main-print number 0-10 usint putchar()
+ *
+ * Return:always 0
+ */
+int main(void)
+	{
+		int i;
+
+		for (i = 0; i < 10; i++)
+		{
+			putchar(i);
+		}
+	putchar('\n');
+	return (0);
+	}
