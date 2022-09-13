3d7b2f49b01ce82481cb4cae0f60e740e21a2bb3
Mackay Brian <brianmackay091@gmail.com>
2022-09-13 03:54:11 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * main-main function
+ * Descriprion: A program that prints _putchar
+ * Return: 0 success
+ * _putchar-prints the out put
+ */
+int main(void)
+{
+int _putchar(char c)
+	{
+		_putchar('_');
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
+		_putchar('\n');
+		return (0);
+	}
+}
