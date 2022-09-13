91a2c3b47830a593e19d25dc10037086abd1987c
Mackay Brian <brianmackay091@gmail.com>
2022-09-13 04:27:01 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * main-main function
+ *
+ * Return:always 0
+ *
+ */
+void print_alphabet(void)
+	{
+		int _putchar()
+		{
+		char c;
+
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');
+		}
+	}
+int main(void)
+{
+	void print_alphabet(void);
+}
