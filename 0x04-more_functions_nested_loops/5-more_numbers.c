b84311badbf752dee0743e6df81cc1e1bd2353b1
Mackay Brian <brianmackay091@gmail.com>
2022-09-15 10:05:54 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,16 +4,15 @@
  */
 void more_numbers(void)
 	{
-		int a;
-		int i;
+		int a, i;
 
-		for (i = 1; i <= 10; i++)
+		for (i = 0; i <= 9; i++)
 		{
 			for (a = 0; a <= 14; a++)
 			{
 				if (a > 9)
 				{
-					_putchar((n / 10) + '0');
+					_putchar((a / 10) + '0');
 				}
 				_putchar((a % 10) + '0');
 			}
