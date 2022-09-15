b71292d9998572c1f4e5fe6a95570a901416b86d
Mackay Brian <brianmackay091@gmail.com>
2022-09-15 12:37:39 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,15 +5,9 @@
  */
 void print_line(int n)
 {
-	int line;
-
-	if (n > 0)
+	while (n-- > 0)
 	{
-		for (line = 0; line <= n; line++)
-		{
-			_putchar('_');
-		}
-		_putchar('\n');
+		_putchar('_');
 	}
-	_putchar('\n');
-}
+	putchar('\n');
+
