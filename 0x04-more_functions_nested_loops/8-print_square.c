d6903788bd5f2cf856718bdb7642877ffa1391d6
Mackay Brian <brianmackay091@gmail.com>
2022-09-15 13:20:47 -0700

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,24 +6,23 @@
  */
 void print_square(int size)
 	{
-		int h, w;
-
-		if (size > 0)
+		if (size <= 0)
 		{
-			for (h = 0; h <= size; h++)
+			_putchar('\n');
+		}
+		else 
+		{
+			int h, w;
+
+			for (h = 0; h < size; h++)
 			{
-				for (w = 0; w <= size; w++)
+				for (w = 0; w < size; w++)
 				{
 					_putchar('#');
-				}
-				if (h == size - 1)
-				{
-					continue;
 				}
 				_putchar('\n');
 			}
 		}
-		_putchar('\n');
 	}
 
 
