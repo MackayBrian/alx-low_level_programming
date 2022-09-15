e401ff9e2769bb2309cdf66d10cdf257c3d08de8
Mackay Brian <brianmackay091@gmail.com>
2022-09-15 06:30:03 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - prints numbers 1 t0 100 but multiple of three,
+ * Fizz is printed, buzz in place of multiples of fives
+ * and fizzbuzz in multiple of both
+ * Return: always 0
+ */
+int main(void)
+	{
+		int i;
+
+		for (i = 1; i <= 100; i++)
+		{
+			if ((i % 3) == 0 && (i % 5) == 0)
+			{
+				printf("FizzBuzz")'
+			}
+			else if ((i % 3) == 0)
+			{
+				printf("Fizz");
+			}
+			else if ((i % 5) == 0)
+			{
+				printf("Buzz");
+			}
+			else
+			{
+				printf("%d", i);
+			}
+			if (n == 100)
+			{
+				continue;
+			}
+			printf(" ");
+		}
+		printf("\n");
+		return (0);
