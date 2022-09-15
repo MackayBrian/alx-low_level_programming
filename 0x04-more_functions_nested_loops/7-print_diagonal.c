27e1192e61cec44bb4865d458b45449779dc4e8d
Mackay Brian <brianmackay091@gmail.com>
2022-09-15 13:11:52 -0700

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,23 +5,31 @@
  */
 void print_diagonal(int n)
 	{
-		int line, gap;
-
-		if (n > 0)
+		if (n <= 0)
 		{
-			for (line = 0; line <= n; line++)
-			{
-				for (gap = 0; gap <= line; gap++)
-				{
-					_putchar(' ');
-				}
-				_putchar('\\');
-				if (line == n - 1)
-				{
-					continue;
-				}
-				_putchar('\n');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
+		else
+		{
+			int line, gap;
+
+			for (line = 0; line < n; line++)
+			{
+				for (gap = 0; gap < n; gap++)
+				{
+					if(line == gap)
+					{
+						_putchar('\\');
+					}
+					else if (gap < line)
+					{
+						_putchar(' ');
+					}
+				}
+			}
+			_putchar('\n');
+		}
 	}
+
+
+
