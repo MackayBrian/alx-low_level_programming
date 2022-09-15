75e2dac3e279b0190dd3574ee4892fe0e108fc77
Mackay Brian <brianmackay091@gmail.com>
2022-09-15 04:12:02 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,11 +5,11 @@
  */
 void print_numbers(void)
 	{
-		int i;
+		int n;
 
-		for (i = 0; i <= 9; i++)
+		for (n = 0; n < 10; n++)
 		{
-			_putchar((i % 10) + '0');
+			_putchar((n % 10) + '0');
 		}
 		_putchar('\n');
 	}
