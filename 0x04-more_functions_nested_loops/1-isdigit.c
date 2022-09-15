8e1c65f93526a82417c9e06f0694db84fc125aed
Mackay Brian <brianmackay091@gmail.com>
2022-09-15 02:39:10 -0700

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isdigit- chaecks if value entered is a didgit or not
+ * @c:value entered by user
+ *
+ * Return: 1 if digit else 0
+ */
+int _isdigit(int c)
+	{
+		char c;
+
+		if (isdigit(c) == 0)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
