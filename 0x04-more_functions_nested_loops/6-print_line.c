fda9f1e5f66e1a55bdc36f6a73859958a0a8d97b
Mackay Brian <brianmackay091@gmail.com>
2022-09-15 05:52:34 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line- draws a straight line using _
+ * @n: the number of _ characters
+ */
+void print_line(int n)
+{
+	int line;
+
+	if (n > 0)
+	{
+		for (line = 0; line <= n; line++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
+
