873ef404d9f59bc03a6bda735100273dc19a3eaf
Mackay Brian <brianmackay091@gmail.com>
2022-09-19 13:35:57 -0700

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * *_strcpy-copies the string pointed to
+ * @dest:destination
+ * @src:source
+ * Return:value at dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (*dest);
+}
