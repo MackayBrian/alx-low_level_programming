c2c77a6f88be431c6227b4e228de3e93f2cbd54c
Mackay Brian <brianmackay091@gmail.com>
2022-09-19 11:39:13 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,13 +7,15 @@ void puts_half(char *str)
 {
 	int len = strlen(str);
 	int i, mid = len / 2;
+	char secondh[10];
 
 	if (len % 2 != 0)
 	{
 		mid = (len - 1) / 2;
+		for (i = 0; i > mid; i++)
+		{
+			str[i] = secondh[10];
+		}
 	}
-	for (i = 0; i > mid; i++)
-	{
-	puts(str[i]);
-	}
+	puts(secondh);
 }
