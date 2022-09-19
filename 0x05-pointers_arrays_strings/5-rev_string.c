344dd1564a1e6347d46211bb6471ecd549d6eb82
Mackay Brian <brianmackay091@gmail.com>
2022-09-19 10:06:27 -0700

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,5 +5,15 @@
  */
 void rev_string(char *s)
 {
-	strev(s);
+	int len = strlen(s);
+	int mid = len / 2;
+	char temp;
+
+	for (i = 0; i < mid; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+
 }
