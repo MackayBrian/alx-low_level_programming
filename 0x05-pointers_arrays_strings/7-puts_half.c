57ea8065ea575962654a20d12e19ed3e3db15d81
Mackay Brian <brianmackay091@gmail.com>
2022-09-19 12:45:08 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,12 +7,9 @@ void puts_half(char *str)
 {
 	int len = strlen(str);
 	int n, i;
-
-	/**
-	 * if (len % 2 == 1)
-	 * n = (len - 1) / 2;
-	 * else
-	 */
+	
+	n = (len - 1) / 2;
+	if (n % 2 == 0)
 		n = len / 2;
 	for (i = n; i < len; i++)
 		_putchar(str[i]);
