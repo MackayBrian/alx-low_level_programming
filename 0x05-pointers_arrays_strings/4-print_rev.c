cddda6370c5faca846402289aa24562745c19ab5
Mackay Brian <brianmackay091@gmail.com>
2022-09-19 05:58:53 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_rev-prints string in a reverse order
+ * @s:string
+ */
+void print_rev(char *s)
+	{
+		int len = strlen(s);
+
+		while (len--)
+			_putchar(*(s + len));
+	}
