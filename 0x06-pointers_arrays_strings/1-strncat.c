cbec263cb23effaf5342c7b8f1499fa2ece2333f
Mackay Brian <brianmackay091@gmail.com>
2022-09-21 02:05:30 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * *_strncat- concatenates two strings
+ * @dest:string destination
+ * @src:string source
+ * @n:number of bytes to be concatenated
+ * Return:pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+	return (dest)
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
 #include <stdlib.h>
