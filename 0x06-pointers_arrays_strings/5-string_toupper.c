b3be41c53704a5a38fef98a558282fb50c45bce6
Mackay Brian <brianmackay091@gmail.com>
2022-09-22 05:28:56 -0700

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,13 +8,12 @@ char *string_toupper(char *s)
 {
 	int i;
 
-	while (s[i] != '\0')
+	while (s[i++])
 	{
 		if (s[i] >= 'a' && s[i] <= 'z')
 		{
 			s[i] = s[i] - 32;
 		}
-		i++;
 	}
 	return (s);
 }
