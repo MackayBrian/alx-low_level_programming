d15105ca6dea854fc0865efb8169a623021d2740
Mackay Brian <brianmackay091@gmail.com>
2022-09-22 05:18:10 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,9 +12,9 @@ int _strcmp(char *s1, char *s2)
 
 	while (i)
 	{
-		if s1[i] == '\0' && s2[i] == '\0')
+		if (s1[i] == '\0' && s2[i] == '\0')
 			break;
-		else if (s[i] == '\0')
+		else if (s1[i] == '\0')
 		{
 			flag = s2[i];
 			break;
