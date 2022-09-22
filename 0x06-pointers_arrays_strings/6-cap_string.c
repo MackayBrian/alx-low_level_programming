76aaa6af74fa44f5e991a1407a1ce978ef88f253
Mackay Brian <brianmackay091@gmail.com>
2022-09-22 06:03:51 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,30 +6,26 @@
  */
 char *string_toupper(char *str)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (str[++i])
 	{
-		if (i == 0)
-		{
-			if ((str[i] >= 'a' && str[i] <= 'z'))
-			{
-				str[i] = str[i] - 32;
-				continue;
-			}
-		}
-		if ((str[i] == (' ' || '\t' || '\n' || 
-						',' || ';' || '.' || 
-						'!' || '?' || '"' || '(' || 
-						')' || '{' || '}')
-					++i;
-					if (arr[i] >= 'a' && arr[i] <= 'z')
-				    str[i] = str[i] - 32;
-			    continue;
-			    }
-			    else
-		if (str[i] >= 'A' && str[i] <= 'Z')
-    str[i] = str[i] + 32;
-    }
-    return (0);
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+		i++;
+		if (str[i - 1] == ' ' ||
+				str[i - 1] == '\t' ||
+				str[i - 1] == '\n' ||
+				str[i - 1] == ',' ||
+				str[i - 1] == ';' ||
+				str[i - 1] == '.' ||
+				str[i - 1] == '!' ||
+				str[i - 1] == '?' ||
+				str[i - 1] == '"' ||
+				str[i - 1] == '(' ||
+				str[i - 1] == ')' ||
+				str[i - 1] == '{' ||
+				str[i - 1] == '}')
+			str[i] -= 32;
+	}
+	return (str);
 }
