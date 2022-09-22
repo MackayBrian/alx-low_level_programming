b5c27565bce5ec338bcccfee3854c4130a3a766b
Mackay Brian <brianmackay091@gmail.com>
2022-09-22 03:21:59 -0700

    task 2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,13 +8,15 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i;
 	int j;
 
-	while (dest[i] != '\0')
-		++i;
-	for (j = 0; src[j] != '\0' && j < n; ++j; ++i)
-		dest[i] = src[j];
+	n = strlen(src);
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; src[j] != '\0' && j < n; i++)
+		dest[i] = src[j++];
 	dest[i] = '0';
 	return (dest);
 }
