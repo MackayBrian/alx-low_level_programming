06d256d5cf8ae76d1abd5ccf3a7828b4da97a359
Mackay Brian <brianmackay091@gmail.com>
2022-09-26 04:23:49 -0700

    task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdbool.h>
+/**
+ * _strspn- gets the length of a prefix substring
+ * @s:main string
+ * @accept:subtring
+ * Return:number of bytes in the initial segment of s which
+ * consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	size_t init_len = 0, i, j;
+	size_t len1 = strlen(s);
+	size_t len2 = strlen(accept);
+
+	for (size_t i = 0; i < len1; i++)
+	{
+		for (size_t j = 0; j < len2; j++)
+			if (accept[j] == s[i])
+			{
+				found_match = true;
+				break;
+			}
+		if (!found_match)
+			break;
+		else
+			init_len++;
+	}
+	return (init_len);
+}
+
