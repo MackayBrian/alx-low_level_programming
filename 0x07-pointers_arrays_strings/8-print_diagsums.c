16b9b03017d3fdf02893e51e3804d1036aedd901
Mackay Brian <brianmackay091@gmail.com>
2022-09-26 06:47:34 -0700

    task 6 and  7

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,20 @@
+#includde "main.h"
+/**
+ * print_diagsums-adds two diagonals of a square matrix
+ * @a:matrix of integers
+ * @size: size of the matrix
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+		sum1 += a[i];
+		a += size;
+	a -= size;
+	for (i = 0; i < size; i++)
+	{
+		sum2 += a[i];
+		a -= size;
+	}
+}
