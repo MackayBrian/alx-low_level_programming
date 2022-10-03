1ca09cbadca8dffd7af907835069ca0d10fc0448
Mackay Brian <brianmackay091@gmail.com>
2022-10-03 06:52:47 -0700

    task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * **alloc_grid-eturns a pointer to a 2 dimensional array of integers.
+ * @width:width of the array
+ * @height:height of the matrix
+ * Return:NULL on failure
+ * if width or height is 0 or negative, return NULL
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j, a, b;
+	int **p;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		p = (int **) malloc(height * sizeof(int *));
+		if (!p)
+		{
+			free(p);
+			return (NULL);
+		}
+		for (i = 0; i < height; i++)
+		{
+			p[i] = (int *) malloc(width + sizeof(int));
+			if (!p[i])
+			{
+				for (j = 0; j <= i; j++)
+					free(p[j]);
+				fee(p);
+				return (NULL);
+			}
+		}
+		for (a = 0;  < height; a++)
+		{
+			for (b = 0; b < width; b++)
+			{
+				b[a][b] = 0;
+			}
+		}
+		return (p);
+	}
+}
