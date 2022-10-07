18c54b14c76f9fc18a55806e82866e77c8f5859a
Mackay Brian <brianmackay091@gmail.com>
2022-10-07 02:27:08 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,40 +8,33 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p;
-	unsigned int so1 = 0, so2 = 0, i;
+	unsigned int i = 0, j = 0, k = 0, l = 0;
+	char *str;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
-	while (s1[so1])
-	{
-		so1++;
-	}
-	while (s2[so2])
-	{
-		so2++;
-	}
-	if (n >= so2)
-	{
-		n = so2;
-		p = malloc((so1 + n + 1) * sizeof(char));
-	}
-	if (p == NULL)
-		return (0);
-	for (i = 0; i < so1; i++)
-	{
-		p[i] = s1[i];
-	}
-	for (; i < (so1 + n); i++)
-	{
-		p[i] = s2[i - so1];
-	}
-	p[i] = '\0';
-	return (p);
+	while (s1[i])
+		i++;
+	while (s2[k])
+		k++;
+	if (n >= k)
+		l= i + k;
+	else
+		l = i + n;
+	str = malloc(sizeof(char) + l + 1);
+	if (str == NULL)
+		return (NULL);
+	k = 0;
+	while (j < l)
+		if (j <= i)
+			str[j] = s1[j];
+		if (j >= i)
+			str[j] = s2[k];
+		k++;
+		j++;
+	str[j] = '\0';
+	return (str);
 }
+	
