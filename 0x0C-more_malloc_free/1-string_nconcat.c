d06b51e1b35e49c29682834416e38ed8a1e0a792
Mackay Brian <brianmackay091@gmail.com>
2022-10-07 02:37:12 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,7 +20,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s2[k])
 		k++;
 	if (n >= k)
-		l= i + k;
+		l = i + k;
 	else
 		l = i + n;
 	str = malloc(sizeof(char) + l + 1);
@@ -28,13 +28,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	k = 0;
 	while (j < l)
+	{
 		if (j <= i)
+		{
 			str[j] = s1[j];
+		}
 		if (j >= i)
+		{
 			str[j] = s2[k];
-		k++;
+			k++;
+		
+		}
+	}
 		j++;
 	str[j] = '\0';
 	return (str);
 }
-	
