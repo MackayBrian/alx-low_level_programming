c4a2a6ca07945fa1fd71ec69f2046c48c1a6964d
Mackay Brian <brianmackay091@gmail.com>
2022-10-11 08:46:04 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog- structure name
+ * @name:name
+ * @age:age
+ * @owner:owner
+ * Decription: dog structure
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /*DOG_H*/
