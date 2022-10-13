06c186504bd2f796f1529624f0b6c8c165a413cc
Mackay Brian <brianmackay091@gmail.com>
2022-10-13 14:16:19 -0700

    task 0

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,27 @@
+#include "3-calc.h"
+/**
+ * get_op_func-selects the correct function to perform the operation
+ * @s: opetator given by user
+ * Return: pointer to function
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	opt_t opts[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_muk},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (ops[i].op)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "calc.h"
+/**
+ * op_add-calculates sum of two integers
+ * @a:fist int
+ * @b:sec int
+ * Return:sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub-calculates the diff of two numbers
+ * @a:first int
+ * @b:sec int
+ * Return: diff of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul-calculates the product of two numbers
+ * @a:first int
+ * @b:sec int
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div-calculates the division of two numbers
+ * @a:first int
+ * @b:sec int
+ * Return: quotient  of a by b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod-calculates the remainder of div of two numbers
+ * @a:first int
+ * @b:sec int
+ * Return: remainder of a / b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
