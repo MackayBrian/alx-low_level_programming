3043cd4a1133f683fb3e37cfc3be3cc179d944b6
Mackay Brian <brianmackay091@gmail.com>
2022-10-14 12:06:22 -0700

    task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers-prints numbers, followed by a new line
+ * @n: number of integers passed
+ * @separator:string to be printed btn numbers
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(list, int));
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(list);
+
+}
