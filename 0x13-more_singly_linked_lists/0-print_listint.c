92ed31bd66731ccb4dc3961b0d6e310d7289c22c
Mackay Brian <brianmackay091@gmail.com>
2022-10-27 06:37:46 -0700

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint-prints all the elements of a listint_t list
+ * @h:element of the list
+ * Return:number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		printf("%d\n", h->data);
+		h = h->next;
+	}
+	return (nodes);
+}
