826500c95ae3129a1e1fa1c4f04ba1b9fba62481
Mackay Brian <brianmackay091@gmail.com>
2022-10-27 08:41:19 -0700

    task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * free_listint-frees a listint_t list.
+ * @head:element of list to be freed
+ * Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head)
+		tmp = head->next;
+		free(head);
+		head = tmp;
+}
