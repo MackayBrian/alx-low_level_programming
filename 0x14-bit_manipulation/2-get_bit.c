b84b988dad144cafd37e7f55c7a9ffed6acad8df
Mackay Brian <Brian Mackay>
2022-10-31 07:20:53 -0700

    task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *  get_bit-returns the value of a bit at a given index
- *  @index: index, starting from 0 of the bit 
- *  @n:the bit
- *  Return:the value of the bit at index index or -1 if an error occured
+ * get_bit-returns the value of a bit at a given index
+ * @index: index, starting from 0 of the bit 
+ * @n:the bit
+ * Return:the value of the bit at index index or -1 if an error occured
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
