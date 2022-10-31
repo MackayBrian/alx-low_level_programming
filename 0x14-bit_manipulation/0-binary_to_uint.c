7f572dd47ec0775be35061a2288ecd6b48485464
Mackay Brian <Brian Mackay>
2022-10-31 06:55:05 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,12 +14,16 @@ unsigned int binary_to_uint(const char *b)
 	int i = 0;
 
 	if (b == NULL)
+	{
 		return (0);
+	}
 	len = strlen(b);
-	for (i = len - 1;i >= 0; i--);
+	for (i = len - 1; i >= 0; i--)
 	{
 		if (b[i] != '0' && b[i] != '1')
+		{
 			return (0);
+		}
 		if (b[i] == '1')
 		{
 			c += k;
@@ -28,6 +32,3 @@ unsigned int binary_to_uint(const char *b)
 	}
 	return (c);
 }
-
-
- 
