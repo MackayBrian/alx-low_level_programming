41c864f34845c2743f402e8c60698877d7dcc01e
Mackay Brian <brianmackay091@gmail.com>
2022-12-10 09:43:08 -0800

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,12 +7,11 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nelem = 0;
-	dlistint_t *tmp = h;
 
-	while (tmp != 0)
+	while (h != 0)
 	{
-		printf("%d", tmp->n);
-		tmp = tmp->next;
+		printf("%d\n", h->n);
+		h = h->next;
 		nelem++;
 	}
 	return (nelem);
