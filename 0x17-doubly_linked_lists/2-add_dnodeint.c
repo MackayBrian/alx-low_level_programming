4801ba52caed238314899e1a713538048805013c
Mackay Brian <brianmackay091@gmail.com>
2022-12-10 11:40:04 -0800

    task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new = malloc(sizeof(dlistint_t));
 	new->n = n;
 	if (new == NULL)
-		free(new)
+		free(new);
 		return (NULL);
 	new->next = (*head);
 	new->prev = NULL;
