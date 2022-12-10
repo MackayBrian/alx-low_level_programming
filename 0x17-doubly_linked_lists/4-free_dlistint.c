d1ab0ceb8d9f794dcd93c2aceddb2aad8d8b49d0
Mackay Brian <brianmackay091@gmail.com>
2022-12-10 12:50:34 -0800

    task 3

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a list
+ * @head: head of the linked list.
+ * Return: no return.
+ */
+
+void free_dlistint(dlistint_t *head);
+{
+	dlistint_t *current;
+
+	while ((current = head) != NULL)
+	{
+		head = head->next;
+		free(current->n);
+		free(current);
+	}
+}
