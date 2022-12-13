3fbafe030f2f9c9b16faf5b256c9c3fe05f50964
Mackay Brian <brianmackay091@gmail.com>
2022-12-13 10:27:03 -0800

    task 1

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 gcc -c *.c -fpic
 gcc *.o -shared -o liball.so
-LD_LIBLARY_PATH=:/root/alx-low_level_programming/0x18-dynamic_libraries  root@6d432d1f4e84:~/alx-low_level_programming/0x18-dynamic_libraries
+export LD_LIBRARY_PATH=:/root/alx-low_level_programming/0x18-dynamic_libraries root@6d432d1f4e84:~/alx-low_level_programming/0x18-dynamic_libraries
